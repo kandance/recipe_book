f01e03a2be6586b73b9fce6d1c8c6b3426800ecb
Kandance Ferguson <kandance.ferguson@gmail.com>
2023-03-24 19:50:30 -0400

    added recipe list functionality to component

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is still a test',
+      'https://suebeehomemaker.com/wp-content/uploads/2021/10/sliced-french-bread.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
